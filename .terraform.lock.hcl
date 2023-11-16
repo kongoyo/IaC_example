be7145f89d16486c09cc83ff0f5bc141f5721730
kongoyo <kongoyo@gmail.com>
2023-11-16 16:10:11 +0800

    new file:   .gitignore 	new file:   .terraform.lock.hcl 	new file:   .terraform/providers/registry.terraform.io/ibm-cloud/ibm/1.59.0/linux_amd64/CHANGELOG.md 	new file:   .terraform/providers/registry.terraform.io/ibm-cloud/ibm/1.59.0/linux_amd64/LICENSE 	new file:   .terraform/providers/registry.terraform.io/ibm-cloud/ibm/1.59.0/linux_amd64/NOTICE 	new file:   .terraform/providers/registry.terraform.io/ibm-cloud/ibm/1.59.0/linux_amd64/README.md 	new file:   .terraform/providers/registry.terraform.io/ibm-cloud/ibm/1.59.0/linux_amd64/provider_metadata.json 	new file:   .terraform/providers/registry.terraform.io/ibm-cloud/ibm/1.59.0/linux_amd64/terraform-provider-ibm_v1.59.0 	modified:   main.tf 	new file:   main.tf.old 	new file:   terraform.tfstate 	deleted:    test_git.tf

---
+++ .terraform.lock.hcl
@@ -0,0 +1,15 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/ibm-cloud/ibm" {
+  version = "1.59.0"
+  hashes = [
+    "h1:aySXR6swwPfHvMpo/qH3uPTq1R5HqbokNOyVEKsKvCI=",
+    "zh:08e24262c41add2c8a86f2718e0edf1b0f608fe3eaf6141ad8310462fbcd3f61",
+    "zh:21417771bf477f6917a65ea1c5fabb58a193ea9be32472f91be9e3a3fdd329f1",
+    "zh:2480336865f2086f11099347da9139bde5c381bd58b3aead8fc252d2797b430b",
+    "zh:62732bc04b32db38deaebc98c0b53406beaf3db265ba2bb3c50520bd852015d9",
+    "zh:6960ee036c1b6a7401998988e1d162627e76a65116a45078602c888baf5a50b7",
+    "zh:6f68511fb175375b14685cdda65f240fb08fea34d35d8e71047f39bb63bc922e",
+  ]
+}
--- main.tf
+++ main.tf
@@ -1,29 +1,10 @@
-# Provider 132
-###################################################################################################
-
 provider "ibm" {
-  region = "us-east"
+  region             = "us-south"
 }
 
-# Resource Group
-###################################################################################################
-
-module "resource_group" {
-  source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.1.0"
-  # if an existing resource group is not set (null) create a new one using prefix
-  resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
-  existing_resource_group_name = var.resource_group
+data "ibm_iam_access_group" "accgroup" {
 }
 
-# COS Instance 
-###################################################################################################
-
-resource "ibm_resource_instance" "cos_instance" {
-  name              = "${var.prefix}-cos"
-  resource_group_id = module.resource_group.resource_group_id
-  service           = "cloud-object-storage"
-  plan              = "standard"
-  location          = "global"
-  tags              = var.resource_tags
+output "accgroups" {
+  value = data.ibm_iam_access_group.accgroup.groups[*].name
 }
