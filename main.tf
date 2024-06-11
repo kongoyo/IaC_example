1fde97cc64f93f5fdf817617624dbafc738a0e1c
kongoyo <87678893+kongoyo@users.noreply.github.com>
2024-06-11 14:15:23 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,65 +1,35 @@
+terraform {
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = "1.40.0" # 確保您使用的是最新版本
+    }
+  }
+}
+
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
+  region = "jp-tok"
 }
 
-// Provision cm_catalog resource instance
-resource "ibm_cm_catalog" "cm_catalog_instance" {
-  label = var.cm_catalog_label
-  short_description = var.cm_catalog_short_description
-  catalog_icon_url = var.cm_catalog_catalog_icon_url
-  tags = var.cm_catalog_tags
+resource "ibm_pi_instance" "ibmi_vsi" {
+  pi_cloud_instance_id  = var.cloud_instance_id
+  pi_sys_type           = "s922"    # 您可以選擇合適的系統類型
+  pi_image_id           = var.image_id
+  pi_network_id         = var.network_id
+  pi_memory             = 16
+  pi_processors         = 0.25
+  pi_proc_type          = "shared"
+  pi_storage_pool       = var.storage_pool
+  pi_key_name           = var.ssh_key_name
 }
 
-// Provision cm_offering resource instance
-resource "ibm_cm_offering" "cm_offering_instance" {
-  catalog_id= var.cm_offering_catalog_id
-  label = var.cm_offering_label
-  offering_icon_url = var.cm_offering_offering_icon_url
-  offering_docs_url = var.cm_offering_offering_docs_url
-  offering_support_url = var.cm_offering_offering_support_url
-  tags = var.cm_offering_tags
-}
-
-// Provision cm_version resource instance
-resource "ibm_cm_version" "cm_version_instance" {
-  catalog_id = var.cm_version_catalog_id
-  offering_id = var.cm_version_offering_id
-  tags = var.cm_version_tags
-  target_kinds = var.cm_version_target_kinds
-  zipurl = var.cm_version_zipurl
-  target_version = var.cm_version_target_version
-}
-
-// Provision cm_offering_instance resource instance
-resource "ibm_cm_offering_instance" "cm_offering_instance_instance" {
-  label = var.cm_offering_instance_label
-  catalog_id = var.cm_offering_instance_catalog_id
-  offering_id = var.cm_offering_instance_offering_id
-  kind_format = var.cm_offering_instance_kind_format
-  version = var.cm_offering_instance_version
-  cluster_id = var.cm_offering_instance_cluster_id
-  cluster_region = var.cm_offering_instance_cluster_region
-  cluster_namespaces = var.cm_offering_instance_cluster_namespaces
-  cluster_all_namespaces = var.cm_offering_instance_cluster_all_namespaces
-}
-
-// Create cm_catalog data source
-data "ibm_cm_catalog" "cm_catalog_instance" {
-  catalog_identifier = var.cm_catalog_catalog_id
-}
-
-// Create cm_offering data source
-data "ibm_cm_offering" "cm_offering_instance" {
-  catalog_id = var.cm_offering_catalog_id
-  offering_id = var.cm_offering_offering_id
-}
-
-// Create cm_version data source
-data "ibm_cm_version" "cm_version_instance" {
-  version_loc_id = var.cm_version_version_loc_id
-}
-
-// Create cm_offering_instance data source
-data "ibm_cm_offering_instance" "cm_offering_instance_instance" {
-  instance_identifier = var.cm_offering_instance_instance_identifier
+resource "ibm_pi_network" "cloud_connection" {
+  name                 = "my_cloud_connection"
+  type                 = "cloud"
+  cloud_instance_id    = var.cloud_instance_id
+  network_cidr         = var.network_cidr
+  speed                = 1000
+  metered              = false
+  global_routing       = false
 }
